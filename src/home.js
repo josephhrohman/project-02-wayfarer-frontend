fb519f5cb490e76c57ec8f68b206a6518393b451
Jhrohman <josephhrohman@gmail.com>
2019-05-02 08:57:05 -0700

    Initial nav items created and rendered

--- src/home.js
+++ src/home.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import Nav from './components/nav';
 import './home.css';
 
 class Home extends Component {
   render() {
     return (
       <div className="container">
-        <h1>HI</h1>
+        <Nav />
       </div>
     );
   }
