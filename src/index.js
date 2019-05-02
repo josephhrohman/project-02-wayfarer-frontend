df0a6c4848a502295abcace506a669c975b26e00
Jhrohman <josephhrohman@gmail.com>
2019-05-02 15:12:23 -0700

    fixing files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
-import Home from './Home';
+import Home from './home';
 
 // ReactDOM.render(<Home />, document.getElementById('root'));
 ReactDOM.render(<Router><Home /></Router>, document.getElementById('root'));
