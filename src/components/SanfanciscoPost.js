c3be4b5d7656b1e2b58fd64ee7e940e373c86eb7
dli24 <dli24@buffalo.edu>
2019-05-02 10:48:53 -0700

    create Sanfrancisco Post

--- src/components/SanfanciscoPost.js
+++ src/components/SanfanciscoPost.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+class Sanfrancisco extends Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+
+
+}
+
+export default Sanfrancisco;
