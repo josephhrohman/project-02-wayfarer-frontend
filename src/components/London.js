6ca1cfdf9acc4f6012c16d12918099f34063016e
dli24 <dli24@buffalo.edu>
2019-05-02 10:47:31 -0700

    create London Post

--- src/components/London.js
+++ src/components/London.js
@@ -1,2 +1,13 @@
 import React, {Component} from 'react';
 
+class London extends Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+
+
+}
+
+export default London;
